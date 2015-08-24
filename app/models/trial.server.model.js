924c5c349131362c31e9945465ec73d48c405bb8
Hongxin Zhang <jackson.zhang.828@gmail.com>
2015-08-24 11:25:09 -0400

    change database

--- app/models/trial.server.model.js
+++ app/models/trial.server.model.js
@@ -39,7 +39,7 @@ var mongoose = require('mongoose'),
 	Schema = mongoose.Schema,
     autoIncrement = require('mongoose-auto-increment');
 
-var connection = mongoose.createConnection('mongodb://localhost/clinicaltrials-dev');
+var connection = mongoose.createConnection('mongodb://localhost/clinicaltrials-test');
 
 autoIncrement.initialize(connection);
 
